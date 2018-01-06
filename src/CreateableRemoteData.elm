3106d2cf5f62eb2acf9402f8c8325b926c350920
SidneyNemzer <sidney@ucode.com>
2018-01-05 16:21:46 -0800

    Implement Createable and Editable RemoteData

--- src/CreateableRemoteData.elm
+++ src/CreateableRemoteData.elm
@@ -1,7 +1,113 @@
-module CreateableRemoteData exposing (..)
+module CreateableRemoteData
+    exposing
+        ( CreateableRemoteData
+        , create
+        , edit
+        , set
+        , delete
+        , unwrapLocalRemote
+        , unwrapLocal
+        , fromResult
+        , upload
+        , uploaded
+        , isSaved
+        , isLoading
+        , isFailed
+        , isSuccess
+        , isUploading
+        )
 
-import EditableRemoteData exposing (EditableRemoteData)
+import RemoteData exposing (RemoteData(..))
+import UploadStatus exposing (UploadStatus(..))
+import EditableRemoteData exposing (EditableRemoteData, LocalRemote)
 
 
 type alias CreateableRemoteData data downloadError uploadError =
     EditableRemoteData (Maybe data) downloadError uploadError
+
+
+create : data -> CreateableRemoteData data de ue
+create data =
+    Success <| ( NotUploading, LocalRemote (Just data) Nothing )
+
+
+edit : (data -> data) -> CreateableRemoteData data de ue -> CreateableRemoteData data de ue
+edit fn =
+    EditableRemoteData.edit
+        (\maybeData ->
+            case maybeData of
+                Just data ->
+                    fn data |> Just
+
+                Nothing ->
+                    Nothing
+        )
+
+
+set : data -> CreateableRemoteData data de ue -> CreateableRemoteData data de ue
+set data =
+    edit (always data)
+
+
+delete : CreateableRemoteData data de ue -> CreateableRemoteData data de ue
+delete =
+    EditableRemoteData.edit (always Nothing)
+
+
+unwrapLocalRemote :
+    a
+    -> (LocalRemote (Maybe data) -> a)
+    -> CreateableRemoteData data de ue
+    -> a
+unwrapLocalRemote =
+    EditableRemoteData.unwrapLocalRemote
+
+
+unwrapLocal : a -> (Maybe data -> a) -> CreateableRemoteData data de ue -> a
+unwrapLocal =
+    EditableRemoteData.unwrapLocal
+
+
+fromResult : Result error data -> CreateableRemoteData data error ue
+fromResult =
+    Result.map Just >> EditableRemoteData.fromResult
+
+
+upload :
+    CreateableRemoteData data de ue
+    -> ( Maybe (LocalRemote (Maybe data)), CreateableRemoteData data de ue )
+upload =
+    EditableRemoteData.upload
+
+
+uploaded :
+    Result uploadError ()
+    -> CreateableRemoteData data de uploadError
+    -> CreateableRemoteData data de uploadError
+uploaded =
+    EditableRemoteData.uploaded
+
+
+isSaved : CreateableRemoteData date de uploadError -> Bool
+isSaved =
+    EditableRemoteData.isSaved
+
+
+isLoading : CreateableRemoteData date de uploadError -> Bool
+isLoading =
+    RemoteData.isLoading
+
+
+isFailed : CreateableRemoteData date de uploadError -> Bool
+isFailed =
+    RemoteData.isFailed
+
+
+isSuccess : CreateableRemoteData date de uploadError -> Bool
+isSuccess =
+    RemoteData.isSuccess
+
+
+isUploading : CreateableRemoteData date de uploadError -> Bool
+isUploading =
+    EditableRemoteData.isUploading
--- src/EditableRemoteData.elm
+++ src/EditableRemoteData.elm
@@ -1,4 +1,19 @@
-module EditableRemoteData exposing (..)
+module EditableRemoteData
+    exposing
+        ( EditableRemoteData
+        , LocalRemote
+        , edit
+        , unwrapLocalRemote
+        , unwrapLocal
+        , fromResult
+        , upload
+        , uploaded
+        , isSaved
+        , isLoading
+        , isFailed
+        , isSuccess
+        , isUploading
+        )
 
 import RemoteData exposing (RemoteData(..))
 import UploadStatus exposing (UploadStatus(..))
@@ -12,3 +27,148 @@ type alias LocalRemote data =
 
 type alias EditableRemoteData data downloadError uploadError =
     RemoteData ( UploadStatus data uploadError, LocalRemote data ) downloadError
+
+
+edit : (data -> data) -> EditableRemoteData data de ue -> EditableRemoteData data de ue
+edit fn =
+    RemoteData.map
+        (Tuple.mapSecond
+            (\localRemote ->
+                LocalRemote
+                    (fn localRemote.local)
+                    localRemote.remote
+            )
+        )
+
+
+unwrapLocalRemote : a -> (LocalRemote data -> a) -> EditableRemoteData data de ue -> a
+unwrapLocalRemote default fn =
+    RemoteData.unwrap
+        default
+        (always default)
+        (\( _, localRemote ) ->
+            fn localRemote
+        )
+
+
+unwrapLocal : a -> (data -> a) -> EditableRemoteData data de ue -> a
+unwrapLocal default fn =
+    unwrapLocalRemote
+        default
+        (.local >> fn)
+
+
+fromResult : Result error data -> EditableRemoteData data error ue
+fromResult =
+    RemoteData.fromResult
+        >> RemoteData.map
+            (\remote -> ( NotUploading, LocalRemote remote remote ))
+
+
+upload :
+    EditableRemoteData data de ue
+    -> ( Maybe (LocalRemote data), EditableRemoteData data de ue )
+upload remoteData =
+    let
+        noUpload =
+            ( Nothing, remoteData )
+    in
+        RemoteData.unwrap
+            noUpload
+            (always noUpload)
+            (\( uploadStatus, { local, remote } as localRemote ) ->
+                if local == remote then
+                    noUpload
+                else
+                    let
+                        willUpload =
+                            ( Just localRemote
+                            , Success <| ( Uploading local, localRemote )
+                            )
+                    in
+                        case uploadStatus of
+                            NotUploading ->
+                                willUpload
+
+                            Uploading data ->
+                                noUpload
+
+                            LastUploadFailed error ->
+                                willUpload
+            )
+            remoteData
+
+
+uploaded :
+    Result uploadError ()
+    -> EditableRemoteData data de uploadError
+    -> EditableRemoteData data de uploadError
+uploaded result =
+    RemoteData.map
+        (\( uploadStatus, { local, remote } as localRemote ) ->
+            case result of
+                Ok () ->
+                    case uploadStatus of
+                        NotUploading ->
+                            ( NotUploading
+                            , localRemote
+                            )
+
+                        Uploading data ->
+                            ( NotUploading
+                            , LocalRemote local data
+                            )
+
+                        LastUploadFailed error ->
+                            ( LastUploadFailed error
+                            , localRemote
+                            )
+
+                Err error ->
+                    ( LastUploadFailed error
+                    , localRemote
+                    )
+        )
+
+
+isSaved : EditableRemoteData date de uploadError -> Bool
+isSaved =
+    RemoteData.unwrap
+        True
+        (always True)
+        (\( uploadStatus, { local, remote } ) ->
+            local == remote
+        )
+
+
+isLoading : EditableRemoteData date de uploadError -> Bool
+isLoading =
+    RemoteData.isLoading
+
+
+isFailed : EditableRemoteData date de uploadError -> Bool
+isFailed =
+    RemoteData.isFailed
+
+
+isSuccess : EditableRemoteData date de uploadError -> Bool
+isSuccess =
+    RemoteData.isSuccess
+
+
+isUploading : EditableRemoteData date de uploadError -> Bool
+isUploading =
+    RemoteData.unwrap
+        True
+        (always True)
+        (\( uploadStatus, _ ) ->
+            case uploadStatus of
+                NotUploading ->
+                    False
+
+                Uploading _ ->
+                    True
+
+                LastUploadFailed _ ->
+                    False
+        )
